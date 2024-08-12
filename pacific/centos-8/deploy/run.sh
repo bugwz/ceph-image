8b11118ff0788d934f6a2f6a6dad4893a6ab7bb1
bugwz <xbugwz@gmail.com>
2024-08-13 00:02:30 +0800

    add scripts to build the pacific version of ceph

---
+++ pacific/centos-8/deploy/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+start_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Build the docker deploy image start: $start_time"
+
+# CEPH_VERSION is a tag or branch name in git
+tag="v16.2.15"
+sudo docker build --build-arg "CEPH_VERSION=$tag" -t "ceph-deploy:centos8-$tag" .
+
+end_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Build the docker deploy image end: $end_time"
