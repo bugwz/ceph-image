0a6422ede954e048a794d5ed571c9e2af5df8823
bugwz <xbugwz@gmail.com>
2025-08-12 20:46:08 +0800

    modify tentacle image name

--- tentacle/centos-9-stream/dev/run.sh
+++ tentacle/centos-9-stream/dev/run.sh
@@ -4,7 +4,7 @@ start_time=$(date +"%Y-%m-%d %H:%M:%S")
 echo "Build the docker dev image start: $start_time"
 
 # CEPH_VERSION is a tag or branch name in git
-tag="v19.2.1"
+tag="tentacle"
 sudo docker build --build-arg "CEPH_VERSION=$tag" -t "ceph-dev:centos9-stream-$tag" .
 
 end_time=$(date +"%Y-%m-%d %H:%M:%S")
